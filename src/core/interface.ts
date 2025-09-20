7aa7626cea8771c67064809e7fd7757b62238ae0
darqus <voladores@inbox.ru>
2025-09-21 01:41:14 +0300

    feat(package): add test:update script for updating snapshots
    - Add new script "test:update" to run unit tests with snapshot update flag
    - Update border color values in interface.ts and theme.snapshot.test.ts.snap
    - Change default border mix ratio from 0.2 to 0.1 for more visible borders

--- src/core/interface.ts
+++ src/core/interface.ts
@@ -39,7 +39,7 @@ export const interfacePalette: InterfacePalette = {
 
   // Границы (автогенерация)
   border: {
-    default: mix(basePalette.black, basePalette.gray, 0.2),
+    default: mix(basePalette.black, basePalette.gray, 0.1),
     focus: withAlpha(basePalette.blue, 0.4),
   },
 
